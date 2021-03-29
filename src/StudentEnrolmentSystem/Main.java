4ffe522d5bd26a03834353133746b118e3b54b54
Hoang Linh <73870812+hlinh1512@users.noreply.github.com>
2021-03-29 12:03:52 +0700

    update case 6 and case 7

--- src/StudentEnrolmentSystem/Main.java
+++ src/StudentEnrolmentSystem/Main.java
@@ -12,29 +12,36 @@ public class Main {
     //Main function
     public static void main(String[] args) {
         //Create sample students information in Student class
-        Student student1 = new Student("s1111111", "Luu Khanh Linh", LocalDate.of(2002, Month.MAY, 25));
-        Student student2 = new Student("s2222222", "Nguyen Khanh Long", LocalDate.of(1998, Month.OCTOBER, 10));
-        Student student3 = new Student("s3333333", "Pham Hong Nhung", LocalDate.of(2000, Month.JUNE, 9));
-        Student student4 = new Student("s4444444", "Nguyen Thi Minh Hien", LocalDate.of(2001, Month.JULY, 10));
+        Student sampleStudent1 = new Student("s1111111", "Luu Khanh Linh", LocalDate.of(2002, Month.MAY, 25));
+        Student sampleStudent2 = new Student("s2222222", "Nguyen Khanh Long", LocalDate.of(1998, Month.OCTOBER, 10));
+        Student sampleStudent3 = new Student("s3333333", "Pham Hong Nhung", LocalDate.of(2000, Month.JUNE, 9));
+        Student sampleStudent4 = new Student("s4444444", "Nguyen Thi Minh Hien", LocalDate.of(2001, Month.JULY, 10));
 
 
         //Create sample courses information in Course class
-        Course course1 = new Course("COSC1111", "Software Architecture: Design & Implementation", 12);
-        Course course2 = new Course("COSC2222", "Software Engineering Fundamentals for IT", 12);
-        Course course3 = new Course("COSC3333", "Software Engineering Project Management", 12);
-        Course course4 = new Course("COSC4444", "Programming Project 1", 12);
+        Course sampleCourse1 = new Course("COSC1111", "Software Architecture: Design & Implementation", 12);
+        Course sampleCourse2 = new Course("COSC2222", "Software Engineering Fundamentals for IT", 12);
+        Course sampleCourse3 = new Course("COSC3333", "Software Engineering Project Management", 12);
+        Course sampleCourse4 = new Course("COSC4444", "Programming Project 1", 12);
 
 
         //Initialize student enrolment object
         StudentEnrolment studentEnrolment = new StudentEnrolment();
 
         //Create sample enrollments
-        StudentEnrolment studentEnrolment1 = new StudentEnrolment(student1, course1, "2021A");
-        studentEnrolment.add(studentEnrolment1);
-        StudentEnrolment studentEnrolment2 = new StudentEnrolment(student1, course2, "2021A");
-        studentEnrolment.add(studentEnrolment2);
-        StudentEnrolment studentEnrolment3 = new StudentEnrolment(student1, course3, "2021A");
-        studentEnrolment.add(studentEnrolment3);
+        StudentEnrolment sampleEnrolment1 = new StudentEnrolment(sampleStudent1, sampleCourse1, "2021A");
+        studentEnrolment.add(sampleEnrolment1);
+        StudentEnrolment sampleEnrolment2 = new StudentEnrolment(sampleStudent1, sampleCourse2, "2021A");
+        studentEnrolment.add(sampleEnrolment2);
+        StudentEnrolment sampleEnrolment3 = new StudentEnrolment(sampleStudent2, sampleCourse3, "2021A");
+        studentEnrolment.add(sampleEnrolment3);
+        StudentEnrolment sampleEnrolment4 = new StudentEnrolment(sampleStudent3, sampleCourse3, "2021A");
+        studentEnrolment.add(sampleEnrolment4);
+        StudentEnrolment sampleEnrolment5 = new StudentEnrolment(sampleStudent4, sampleCourse3, "2021A");
+        studentEnrolment.add(sampleEnrolment5);
+        StudentEnrolment sampleEnrolment6 = new StudentEnrolment(sampleStudent1, sampleCourse3, "2021A");
+        studentEnrolment.add(sampleEnrolment6);
+
 
 
 
@@ -44,16 +51,16 @@ public class Main {
         ArrayList<Course> courseList = new ArrayList<>();
 
         //Add students to the list
-        studentList.add(student1);
-        studentList.add(student2);
-        studentList.add(student3);
-        studentList.add(student4);
+        studentList.add(sampleStudent1);
+        studentList.add(sampleStudent2);
+        studentList.add(sampleStudent3);
+        studentList.add(sampleStudent4);
 
         //Add Courses to the list
-        courseList.add(course1);
-        courseList.add(course2);
-        courseList.add(course3);
-        courseList.add(course4);
+        courseList.add(sampleCourse1);
+        courseList.add(sampleCourse2);
+        courseList.add(sampleCourse3);
+        courseList.add(sampleCourse4);
 
 
 
@@ -83,7 +90,7 @@ public class Main {
 
             //if else for the option choice
             switch (option) {
-                //case 1 (Create a student information)
+                //case 1 (Create a new student information)
                 case "1":
                     System.out.print("Create a student information");
                     //Get student information from user input
@@ -100,7 +107,7 @@ public class Main {
                     studentList.add(student);
 
 
-                //case 2 (Create a course information)
+                //case 2 (Create a new course information)
                 case "2":
                     System.out.print("Create a course information");
                     //Get course information from user input
@@ -217,7 +224,7 @@ public class Main {
                             }
                             System.out.println("Deleted successfully!!!");
                         }
-                        //Update the course option (Thiếu name và number of credits)
+                        //Update the course option
                         else if (userAns.equals("3")) {
                             System.out.print("Course ID need to update: ");
                             String courseIdNeedToUpdate = scan.next();
@@ -247,9 +254,55 @@ public class Main {
                         }
 
                     }
-                    //Print all courses for 1 student in 1 semester
+                //Print all courses for 1 student in 1 semester
                 case "5":
                     System.out.println("Display all courses for 1 student in 1 semester");
+
+
+                //Print all students of 1 course in 1 semester
+                case "6":
+                    System.out.print("Enter the course ID you want to check: ");
+                    String courseIDOfAllStudents = scan.next();
+                    System.out.print("Semester: ");
+                    String semesterOfAllStudents = scan.next();
+                    //Array list to store all students of user input course and semester
+                    ArrayList<String> studentOfCourseAndSemester = new ArrayList<>();
+                    //For loop to add all students from enrolment list to the list
+                    for (int i = 0; i < studentEnrolment.enrolmentList.size(); i++){
+                        if (studentEnrolment.enrolmentList.get(i).getCourse().getId().equals(courseIDOfAllStudents)
+                        && studentEnrolment.enrolmentList.get(i).getSemester().equals(semesterOfAllStudents)){
+                            studentOfCourseAndSemester.add(studentEnrolment.enrolmentList.get(i).getStudent().toString());
+                        }
+                    }
+                    //Display
+                    System.out.println("All students of course " + courseIDOfAllStudents + " of semester "+ semesterOfAllStudents + ": ");
+                    for (int i = 0; i < studentOfCourseAndSemester.size(); i++){
+                        System.out.println(studentOfCourseAndSemester.get(i));
+                    }
+
+                case "7":
+                    //Print all courses offered in 1 semester
+                    System.out.print("Enter the semester you want to check: ");
+                    String semesterAllCourse = scan.next();
+                    //Arraylist to store all courses
+                    ArrayList<String> allCoursesInSemester = new ArrayList<>();
+                    for (int i = 0; i < studentEnrolment.enrolmentList.size(); i++){
+                        if (studentEnrolment.enrolmentList.get(i).getSemester().equals(semesterAllCourse)){
+                            allCoursesInSemester.add(studentEnrolment.enrolmentList.get(i).getCourse().toString());
+
+                        }
+                    }
+                    //Remove duplicate courses
+                    ArrayList<String> removeDuplicated = new ArrayList<>();
+                    for (String element : allCoursesInSemester){
+                        if (!removeDuplicated.contains(element)){
+                            removeDuplicated.add(element);
+                        }
+                    }
+                    System.out.println("All courses of semester "+ semesterAllCourse + ": ");
+                    for (int i = 0; i<removeDuplicated.size(); i++){
+                        System.out.println(removeDuplicated.get(i));
+                    }
 
             }
 
